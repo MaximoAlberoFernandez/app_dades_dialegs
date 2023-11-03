43e03405cca21acb9c49e929359b0333619885e5
MaximoAlberoFernandez <maximo.alberofernandez@gmail.com>
2023-11-03 15:48:14 +0100

    Added Alert Dialog

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key); // Agrega 'Key? key' como parámetro del constructor.
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({Key? key}) : super(key: key); // Agrega 'Key? key' como parámetro del constructor.
+  const MyCustomForm({Key? key}) : super(key: key);
 
   @override
   State<MyCustomForm> createState() => _MyCustomFormState();
@@ -34,7 +34,7 @@ class _MyCustomFormState extends State<MyCustomForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Recuperar el valor d'un camp de text"), // Corrige el título aquí.
+        title: const Text("Recuperar el valor d'un camp de text"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -42,28 +42,58 @@ class _MyCustomFormState extends State<MyCustomForm> {
           controller: myController,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            barrierDismissible: true,
-            builder: (context) {
-              return SimpleDialog(
-                title: const Text('Choose your option'),
-                children: [
-                  SimpleDialogOption(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(myController.text), // Corrige el texto aquí.
-                  ),
-                ],
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                barrierDismissible: true,
+                builder: (context) {
+                  return SimpleDialog(
+                    title: const Text('Choose your option'),
+                    children: [
+                      SimpleDialogOption(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(myController.text),
+                      ),
+                    ],
+                  );
+                },
               );
             },
-          );
-        },
-        tooltip: 'Mostra el valor!',
-        child: const Icon(Icons.text_fields),
+            tooltip: 'Mostra el valor!',
+            child: const Icon(Icons.text_fields),
+          ),
+          const SizedBox(height: 16), // Espacio entre los botones.
+          OutlinedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                barrierDismissible: true,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text('Texto en el campo de texto'),
+                    content: Text(myController.text),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('Cerrar'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: const Text('Mostrar Texto'),
+          ),
+        ],
       ),
     );
   }
