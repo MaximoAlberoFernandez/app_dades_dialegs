bb578d5e3f81b198bfcb4a5341d0bd563b00de61
MaximoAlberoFernandez <maximo.alberofernandez@gmail.com>
2023-11-03 16:36:37 +0100

    Divided buttons in two rows

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Recuperar el valor d' 'un camp de text',
+      title: 'Recuperar el valor d\'un camp de text',
       home: MyCustomForm(),
     );
   }
@@ -33,7 +33,7 @@ class _MyCustomFormState extends State<MyCustomForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('recuperar el valor d\'un camp de text')
+        title: const Text('Recuperar el valor d\'un camp de text'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -41,105 +41,118 @@ class _MyCustomFormState extends State<MyCustomForm> {
           controller: myController,
         ),
       ),
-      floatingActionButton: Row(
+      floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          const SizedBox(height: 16), // Espacio entre los botones.
-          OutlinedButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                barrierDismissible: true,
-                builder: (context) {
-                  return AlertDialog(
-                    title: const Text('AlertDialog'),
-                    content: Text(myController.text),
-                    actions: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Tancar'),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-            child: const Text('Alert Dialog'),
-          ),
-          TextButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                barrierDismissible: true,
-                builder: (context) {
-                  return SimpleDialog(
-                    title: const Text('SimpleDialog'),
-                    children: [
-                      SimpleDialogOption(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Text(myController.text),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-            child: const Text('Simple Dialog')  
-          ),
-          OutlinedButton(
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                 content: Text(myController.text),
-                 duration: const Duration(seconds: 2),
-                 ),
-              );
-            }, 
-            child: const Text('SnackBar'),
-            ),
-          TextButton(
-            onPressed: () {
-              showModalBottomSheet(
-                backgroundColor: Colors.transparent,
-                context: context, 
-                builder: (BuildContext context) {
-                  return Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight: Radius.circular(25),
-                      ),
-                    ),
-                    height: 200,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(myController.text),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              OutlinedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    barrierDismissible: true,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('AlertDialog'),
+                        content: Text(myController.text),
+                        actions: [
                           ElevatedButton(
-                            child: const Text('Close BottomSheet'),
-                            onPressed: () => Navigator.of(context).pop(),
-                            )
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Tancar'),
+                          ),
                         ],
-                      ),
-                    )
+                      );
+                    },
                   );
                 },
-              );
-            }, 
-            child: const Text('modalBottomSheet'),
+                child: const Text('Alert Dialog'),
+              ),
+              TextButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    barrierDismissible: true,
+                    builder: (context) {
+                      return SimpleDialog(
+                        title: const Text('SimpleDialog'),
+                        children: [
+                          SimpleDialogOption(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(myController.text),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Text('Simple Dialog'),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              OutlinedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(myController.text),
+                      duration: const Duration(seconds: 2),
+                    ),
+                  );
+                },
+                child: const Text('SnackBar'),
+              ),
+              TextButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                    backgroundColor: Colors.transparent,
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(25),
+                            topRight: Radius.circular(25),
+                          ),
+                        ),
+                        height: 200,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Text(myController.text),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: Color(0xFF42A5F5),
+                                ),
+                                child: const Text('Close BottomSheet'),
+                                onPressed: () => Navigator.of(context).pop(),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: const Text('Modal BottomSheet'),
+              ),
+            ],
           ),
           FloatingActionButton(
             onPressed: () {
               showDialog(
-                context: context, 
+                context: context,
                 builder: (context) {
                   return AlertDialog(
                     title: const Text('Alert Dialog'),
@@ -148,7 +161,7 @@ class _MyCustomFormState extends State<MyCustomForm> {
                 },
               );
             },
-            tooltip: 'Mostra el valor!',
+            tooltip: 'Mostrar el valor!',
             child: const Icon(Icons.text_fields),
           ),
         ],
